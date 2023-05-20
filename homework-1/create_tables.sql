2f8b1895e296ba063aa8c6eab0ec4e8fa383e185
Oleg <kirbenev@bk.ru>
2023-05-20 15:34:57 +0300

    homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50) NOT NULL,
+	birh_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers_data
+(
+	customer_id varchar(50) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,51 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv, psycopg2
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='256987')
+
+with open('north_data/customers_data.csv') as f:
+    items = csv.DictReader(f)
+    cur = conn.cursor()
+    for item in items:
+        cur.execute("INSERT INTO customers_data VALUES (%s, %s, %s)", (item['customer_id'],
+                                                                     item['company_name'],
+                                                                     item['contact_name']))
+    conn.commit()
+    cur.close()
+
+
+with open('north_data/employees_data.csv') as f:
+    items = csv.DictReader(f)
+    cur = conn.cursor()
+    employee_id = 1
+    for item in items:
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (
+            employee_id,
+            item['first_name'],
+            item['last_name'],
+            item['title'],
+            item['birth_date'],
+            item['notes']
+        ))
+
+        employee_id = employee_id + 1
+
+    conn.commit()
+    cur.close()
+
+with open('north_data/orders_data.csv') as f:
+    items = csv.DictReader(f)
+    cur = conn.cursor()
+    for item in items:
+        cur.execute("INSERT INTO orders_data VALUES (%s, %s, %s, %s, %s)", (
+            (item['order_id']),
+            item['customer_id'],
+            item['employee_id'],
+            item['order_date'],
+            item['ship_city']
+        ))
+
+    conn.commit()
+    cur.close()
+
+conn.close()
